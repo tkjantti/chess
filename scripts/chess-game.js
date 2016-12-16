2baa8927c2091c2cd775d187f5f41231b49741f8
Tero Jäntti <tkjantti@gmail.com>
2016-12-16 22:02:42 +0200

    Rules object.

--- scripts/chess-game.js
+++ scripts/chess-game.js
@@ -3,6 +3,7 @@ chessGame = function() {
     var initialized = false;
     var currentPlayer = "white";
     var selectedSquare = null;
+    var rules = createRules();
 
     function createBoardFromPage() {
         var board = createBoard();
@@ -101,13 +102,13 @@ chessGame = function() {
             selectSquare(square);
         }
 
-        if (! isLegalMove(board, selectedPiece, selectedPosition, position)) {
+        if (! rules.isLegalMove(board, selectedPiece, selectedPosition, position)) {
             return;
         }
         
         board.move(selectedPosition, position);
         
-        if (isInCheck(board, currentPlayer)) {
+        if (rules.isInCheck(board, currentPlayer)) {
             var positionOfKing = board.getPositionOf(createPiece(currentPlayer, "king"));
             highlightPieceUnderThreat(positionOfKing);
             return;
@@ -140,135 +141,6 @@ chessGame = function() {
         return parts[1];
     }
 
-    function isInCheck(board, currentPlayer) {
-        var opponent = opponentPlayer(currentPlayer);
-        var positionOfKing = board.getPositionOf(createPiece(currentPlayer, "king"));
-        var attackingPiece = board.findPiece(function (piece, position) {
-            return (piece.player === opponent) && isLegalMove(board, piece, position, positionOfKing);
-        });
-        return attackingPiece;
-    }
-
-    function isLegalMove(board, piece, source, destination) {
-        if (! board.isInside(destination)) {
-            return false;
-        }
-
-        var pieceAtDestination = board.getPiece(destination);
-        if (pieceAtDestination && pieceAtDestination.player === piece.player) {
-            return false;
-        }
-        
-        var horizontal = getHorizontalMovement(source, destination);
-        var vertical = getVerticalMovement(source, destination, piece.player);
-
-        switch (piece.type) {
-        case "pawn":
-            var isFirstMove = getVerticalPosition(piece.player, source) === 1;
-            var isCorrectLengthForwardMove = (vertical === 1) || (isFirstMove && vertical === 2);
-            
-            if (isCorrectLengthForwardMove && isVerticalMove(board, source, destination) && !pieceAtDestination) {
-                return true;
-            }
-
-            if ((horizontal === -1 || horizontal === 1) && vertical === 1 && pieceAtDestination) {
-                return true;
-            }
-            
-            return false;
-
-        case "knight":
-            return (Math.abs(horizontal) === 1 && Math.abs(vertical) === 2)
-                || (Math.abs(horizontal) === 2 && Math.abs(vertical) === 1);
-
-        case "bishop":
-            return isDiagonalMove(board, source, destination);
-            
-        case "rook":
-            return isHorizontalMove(board, source, destination) || isVerticalMove(board, source, destination);
-
-        case "queen":
-            return isDiagonalMove(board, source, destination)
-                || isHorizontalMove(board, source, destination)
-                || isVerticalMove(board, source, destination);
-            
-        case "king":
-            return (Math.abs(horizontal) <= 1 && Math.abs(vertical) <= 1);
-            
-        default:
-            console.log("unknown piece type: " + piece.type);
-            return false;
-        };
-    }
-
-    function isDiagonalMove(board, source, destination) {
-        if (source.row === destination.row) {
-            return false;
-        }
-
-        if (Math.abs(destination.column - source.column) != Math.abs(destination.row - source.row)) {
-            return false;
-        }
-
-        var leftmostPoint = (source.column < destination.column) ? source : destination;
-        var rightmostPoint = (source.column > destination.column) ? source : destination;
-        var rowStep = (rightmostPoint.row > leftmostPoint.row) ? 1 : -1;
-
-        for (var r = leftmostPoint.row + rowStep, c = leftmostPoint.column + 1;
-             c < rightmostPoint.column;
-             r += rowStep, c++)
-        {
-            if (board.getPiece(createPoint(r, c))) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    function isHorizontalMove(board, source, destination) {
-        if (source.row != destination.row) {
-            return false;
-        }
-
-        var min = Math.min(source.column, destination.column);
-        var max = Math.max(source.column, destination.column);
-
-        for (var i = min + 1; i < max; i++) {
-            if (board.getPiece(createPoint(source.row, i))) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    function isVerticalMove(board, source, destination) {
-        if (source.column != destination.column) {
-            return false;
-        }
-
-        var min = Math.min(source.row, destination.row);
-        var max = Math.max(source.row, destination.row);
-
-        for (var i = min + 1; i < max; i++) {
-            if (board.getPiece(createPoint(i, source.column))) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-
-    function getVerticalPosition(player, position) {
-        if (player === "white") {
-            return 7 - position.row;
-        } else {
-            return position.row;
-        }
-    }
-
     function getPiece(board, position) {
         if (! board.isInside(position)) {
             return null;
@@ -282,25 +154,8 @@ chessGame = function() {
         return $("#square_" + position.row + "_" + position.column);
     }
 
-    function getHorizontalMovement(source, destination) {
-        return destination.column - source.column;
-    }
-
-    function getVerticalMovement(source, destination, player) {
-        if (player === "white") {
-            return source.row - destination.row;
-        } else {
-            return destination.row - source.row;
-        }
-    }
-
-
     function changePlayer() {
-        setCurrentPlayer(opponentPlayer(currentPlayer));
-    }
-
-    function opponentPlayer(player) {
-        return (player === "white") ? "black" : "white";
+        setCurrentPlayer(rules.opponentPlayer(currentPlayer));
     }
 
     function move(square, piece) {
---
+++ scripts/chess-rules.js
@@ -0,0 +1,150 @@
+
+var createRules = function() {
+
+    var getVerticalPosition = function(player, position) {
+        if (player === "white") {
+            return 7 - position.row;
+        } else {
+            return position.row;
+        }
+    };
+
+    var getVerticalMovement = function(source, destination, player) {
+        if (player === "white") {
+            return source.row - destination.row;
+        } else {
+            return destination.row - source.row;
+        }
+    };
+
+    var getHorizontalMovement = function(source, destination) {
+        return destination.column - source.column;
+    };
+
+    var isHorizontalMove = function(board, source, destination) {
+        if (source.row != destination.row) {
+            return false;
+        }
+
+        var min = Math.min(source.column, destination.column);
+        var max = Math.max(source.column, destination.column);
+
+        for (var i = min + 1; i < max; i++) {
+            if (board.getPiece(createPoint(source.row, i))) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+
+    var isVerticalMove = function(board, source, destination) {
+        if (source.column != destination.column) {
+            return false;
+        }
+
+        var min = Math.min(source.row, destination.row);
+        var max = Math.max(source.row, destination.row);
+
+        for (var i = min + 1; i < max; i++) {
+            if (board.getPiece(createPoint(i, source.column))) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+    
+    var isDiagonalMove = function(board, source, destination) {
+        if (source.row === destination.row) {
+            return false;
+        }
+
+        if (Math.abs(destination.column - source.column) != Math.abs(destination.row - source.row)) {
+            return false;
+        }
+
+        var leftmostPoint = (source.column < destination.column) ? source : destination;
+        var rightmostPoint = (source.column > destination.column) ? source : destination;
+        var rowStep = (rightmostPoint.row > leftmostPoint.row) ? 1 : -1;
+
+        for (var r = leftmostPoint.row + rowStep, c = leftmostPoint.column + 1;
+             c < rightmostPoint.column;
+             r += rowStep, c++)
+        {
+            if (board.getPiece(createPoint(r, c))) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+    
+    return {
+        opponentPlayer: function(player) {
+            return (player === "white") ? "black" : "white";
+        },
+
+        isInCheck: function(board, currentPlayer) {
+            var opponent = this.opponentPlayer(currentPlayer);
+            var positionOfKing = board.getPositionOf(createPiece(currentPlayer, "king"));
+            var that = this;
+            var attackingPiece = board.findPiece(function (piece, position) {
+                return (piece.player === opponent) && that.isLegalMove(board, piece, position, positionOfKing);
+            });
+            return attackingPiece;
+        },
+
+        isLegalMove: function(board, piece, source, destination) {
+            if (! board.isInside(destination)) {
+                return false;
+            }
+
+            var pieceAtDestination = board.getPiece(destination);
+            if (pieceAtDestination && pieceAtDestination.player === piece.player) {
+                return false;
+            }
+            
+            var horizontal = getHorizontalMovement(source, destination);
+            var vertical = getVerticalMovement(source, destination, piece.player);
+
+            switch (piece.type) {
+            case "pawn":
+                var isFirstMove = getVerticalPosition(piece.player, source) === 1;
+                var isCorrectLengthForwardMove = (vertical === 1) || (isFirstMove && vertical === 2);
+                
+                if (isCorrectLengthForwardMove && isVerticalMove(board, source, destination) && !pieceAtDestination) {
+                    return true;
+                }
+
+                if ((horizontal === -1 || horizontal === 1) && vertical === 1 && pieceAtDestination) {
+                    return true;
+                }
+                
+                return false;
+
+            case "knight":
+                return (Math.abs(horizontal) === 1 && Math.abs(vertical) === 2)
+                    || (Math.abs(horizontal) === 2 && Math.abs(vertical) === 1);
+
+            case "bishop":
+                return isDiagonalMove(board, source, destination);
+                
+            case "rook":
+                return isHorizontalMove(board, source, destination) || isVerticalMove(board, source, destination);
+
+            case "queen":
+                return isDiagonalMove(board, source, destination)
+                    || isHorizontalMove(board, source, destination)
+                    || isVerticalMove(board, source, destination);
+                
+            case "king":
+                return (Math.abs(horizontal) <= 1 && Math.abs(vertical) <= 1);
+                
+            default:
+                console.log("unknown piece type: " + piece.type);
+                return false;
+            };
+        }
+    };
+};
