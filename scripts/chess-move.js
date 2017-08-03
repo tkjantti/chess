66106d2813565465555f84019251cad30b4a352e
Tero Jäntti <tkjantti@gmail.com>
2017-08-03 16:29:09 +0300

    ActualMove inherits Move.
    Also, ActualMove moved to the same file with Move, just to keep it simple.

--- scripts/chess-move.js
+++ scripts/chess-move.js
@@ -39,3 +39,12 @@ CHESS_APP.Move.prototype.isDiagonal = function () {
     return (this.source.row !== this.destination.row) &&
             Math.abs(this.destination.column - this.source.column) === Math.abs(this.destination.row - this.source.row);
 };
+
+CHESS_APP.ActualMove = function (piece, source, destination) {
+    "use strict";
+    CHESS_APP.Move.call(this, source, destination);
+    this.piece = piece;
+};
+
+CHESS_APP.ActualMove.prototype = Object.create(CHESS_APP.Move.prototype);
+CHESS_APP.ActualMove.prototype.constructor = CHESS_APP.Move;
