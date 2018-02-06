a5ec6a7c1c4a920a1fcc290bd5dc86485515c129
Tero Jäntti <tkjantti@gmail.com>
2018-02-06 19:18:28 +0200

    lint:js gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,27 @@
 
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+var jshint = require('gulp-jshint');
+
+
+function customPlumber(errTitle) {
+    return plumber({
+        errorHandler: notify.onError({
+            title: errTitle || "Error running Gulp",
+            message: "Error: <%= error.message %>"
+        })
+    });
+}
+
+gulp.task('lint:js', function () {
+    return gulp.src('src/js/**/*.js')
+        .pipe(customPlumber('JSHint Error'))
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(jshint.reporter('fail'));
+});
 
 gulp.task('watch', ['browserSync'], function () {
     gulp.watch('src/*.html', browserSync.reload);
