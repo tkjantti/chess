fdd8e48bf4dc514f4349b572e0d71e2880d763fe
Tero Jäntti <tkjantti@gmail.com>
2018-02-06 19:19:06 +0200

    Fixed warning: Redefinition of 'CHESS_APP'.

--- src/js/chess-InspectionResult.js
+++ src/js/chess-InspectionResult.js
@@ -1,15 +1,16 @@
 
-var CHESS_APP = CHESS_APP || {};
-
-CHESS_APP.CASTLING_NONE = 0;
-CHESS_APP.CASTLING_KING_SIDE = 1;
-CHESS_APP.CASTLING_QUEEN_SIDE = 2;
-
-CHESS_APP.InspectionResult = function (isLegal, actualMoves) {
+(function (exports) {
     "use strict";
-    this.actualMoves = actualMoves ? actualMoves : [];
-    this.isLegal = isLegal;
-    this.capturePosition = null;
-    this.promotion = null;
-    this.castling = CHESS_APP.CASTLING_NONE;
-};
+
+    exports.CASTLING_NONE = 0;
+    exports.CASTLING_KING_SIDE = 1;
+    exports.CASTLING_QUEEN_SIDE = 2;
+
+    exports.InspectionResult = function (isLegal, actualMoves) {
+        this.actualMoves = actualMoves ? actualMoves : [];
+        this.isLegal = isLegal;
+        this.capturePosition = null;
+        this.promotion = null;
+        this.castling = exports.CASTLING_NONE;
+    };
+})(this.CHESS_APP = this.CHESS_APP || {});
